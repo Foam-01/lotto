f6c5ac9997ec1707844eb79262ba7d0238353665
Foam <ddotdka4@gmail.com>
2026-04-18 11:02:22 +0700

    feat: integrate axios for API requests in App component

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const api = "http://localhost:3000/"; // เติม / ปิดท้าย
+  const handleGet = async () => {
+    await axios
+      .get(api + "api/app") // แก้ให้ตรงกับ @Controller ใน Backend
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  };
+
+  const handlePost = async () => {
+    await axios
+      .post(api + "api/app", { name: "new record" })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  };
+
+  const handlePut = async () => {
+    await axios.put(api + 'api/app/2', { name: 'update edit'}).then(res => {
+      console.log(res.data);
+    }).catch(err => {
+      console.log('Error:', err);
+    });
+  };
+
+  const handleDelete = async () => {
+    await axios.delete(api + 'api/app/3').then(res => {
+      console.log(res.data);
+    }).catch(err => {
+      console.log('Error:', err);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>Connect API</h3>
+
+        <button onClick={handleGet}>GET</button>
+
+        <button onClick={handlePost}>POST</button>
+
+        <button onClick={handlePut}>PUT</button>
+
+        <button onClick={handleDelete}>DELETE</button>
+      </div>
     </div>
   );
 }
