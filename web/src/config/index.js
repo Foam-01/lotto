206f136c6b551e26184e5f96c381f509792298a1
Foam <ddotdka4@gmail.com>
2026-05-21 10:10:54 +0700

    fix: update apiPath to use environment variable for better configuration

--- web/src/config/index.js
+++ web/src/config/index.js
@@ -1,5 +1,6 @@
 const config = {
-  apiPath: "http://localhost:3000",
+  // บอก React ว่า: ถ้ามีตัวแปรบน Cloud ให้ใช้บน Cloud ถ้าไม่มีให้ใช้ localhost  จุด 1
+  apiPath: process.env.REACT_APP_API_URL || "http://localhost:3000",
   headers: () => {
     return {
       headers: {
